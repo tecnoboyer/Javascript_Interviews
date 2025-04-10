cc2dc0461629cdb8af56ee76cae30f530b23fe38
tecnoboyer <ingenieroboyer@gmail.com>
2025-04-10 14:49:20 -0400

    strings

---
+++ Literals/templateLiterals2.js
@@ -0,0 +1,12 @@
+const itemName = 'Smartphone';
+const itemDescription = 'The latest model with advanced features.';
+
+const htmlTemplate = `
+  <div class="item">
+    <h2>${itemName}</h2>
+    <p>${itemDescription}</p>
+  </div>
+`;
+
+
+console.log (htmlTemplate)
