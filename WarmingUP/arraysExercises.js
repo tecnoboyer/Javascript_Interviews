91be4e030ffb155f5046273afa984d159201dad1
tecnoboyer <ingenieroboyer@gmail.com>
2025-04-24 09:33:50 -0400

    24Completed

---
+++ WarmingUP/arraysExercises.js
@@ -0,0 +1,17 @@
+// 5
+// Step 21
+// Arrays are special in that they are considered mutable. This means you can change the value at an index directly.
+
+// For example, this code would assign the number 25 to the second element in the array:
+
+// Example Code
+
+// let rows = ["Naomi", "Quincy", "CamperChan"];
+// rows[2]=10;
+// console.log(rows[2]);
+// Update the third element of your rows array to be the number 10. Then print the rows array to your console.
+
+let cities = [ "London","New York","Mumbai"];
+console.log(cities);
+cities[2]="Mexico City";
+console.log(cities);
