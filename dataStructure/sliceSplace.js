e65173828fec15c0de97a485922acbfab4c65b1b
tecnoboyer <ingenieroboyer@gmail.com>
2025-08-27 18:11:46 -0400

    beforeMAP

---
+++ dataStructure/sliceSplace.js
@@ -0,0 +1,8 @@
+var numbers = [10, 20, 30, 40, 50];
+numbers.splice(3, 1);
+// const slicedNumbers = numbers.slice(2, 4);
+// console.log(numbers.splice(3, 1));
+// console.log(slicedNumbers);
+var number2 = [10, 20, 30, 40, 50];
+var slicednumber2 = number2.slice(2, 4);
+console.log(slicednumber2);
---
+++ dataStructure/sliceSplace.ts
@@ -0,0 +1,14 @@
+const numbers = [10, 20, 30, 40, 50];
+numbers.splice(3, 1);
+// const slicedNumbers = numbers.slice(2, 4);
+
+// console.log(numbers.splice(3, 1));
+// console.log(slicedNumbers);
+
+
+
+const number2 = [10, 20, 30, 40, 50];
+
+const slicednumber2 = number2.slice(2, 4);
+
+console.log(slicednumber2);
