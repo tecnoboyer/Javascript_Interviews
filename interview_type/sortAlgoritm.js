f11fcc3176fb035f94e9b1b2e0386a0efbbe5300
tecnoboyer <ingenieroboyer@gmail.com>
2025-09-26 13:29:42 -0400

    commented

--- interview_type/sortAlgoritm.js
+++ interview_type/sortAlgoritm.js
@@ -1,3 +1,15 @@
+
+// Step-by-Step Breakdown
+// Initialize the Subsets: The process begins by establishing two subsets within the array: one sorted and one unsorted. Initially, the sorted subset is empty, and the entire array constitutes the unsorted subset.
+
+// Iterate Through the Unsorted Part: A main loop iterates through the entire array, with a pointer marking the boundary between the sorted and unsorted subsets. At each iteration, this pointer is the starting element of the current unsorted portion.
+
+// Find the Minimum Value: Within the unsorted subset (which starts at the current loop index), the algorithm scans for the element with the minimum value.
+
+// Swap the Elements: Once the minimum value is found, it is swapped with the element at the current loop index. This moves the smallest element from the unsorted portion into its correct position within the sorted subset.
+
+// Advance the Boundary: The loop then moves to the next element, advancing the boundary between the sorted and unsorted subsets. The process is repeated until the entire array is sorted.
+
 function selectionSort(array) {
     for( let i = 0; i < array.length; i++ ) {
         let swapped;
